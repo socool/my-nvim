805c747eeb9937a7c2e4fc83bf6b40f137f52c6d
So Cool <127.0.0.1.pm@gmail.com>
2023-08-15 11:27:06 +0700

    change file extension

--- lua/core/plugin_config/whichkey.lua
+++ lua/core/plugin_config/whichkey.lua
@@ -1,28 +1,54 @@
-local  whichkey= require("which-key")
+local whichkey = require("which-key")
 local conf = {
-  window = {
-	  border = "single",
-	  position = "bottom"
-  },
-  triggers = {"<leader>"},
-  triggers_nowait = {
-    -- marks
-    "~",
-    "`"
-  }
+	window = {
+		border = "single",
+		position = "bottom",
+	},
+	triggers = { "<leader>" },
+	triggers_nowait = {
+		-- marks
+		"~",
+		"`",
+	},
+	plugins = {
+		presets = {
+			g = true,
+		},
+	},
 }
 
 local opts = {
-  mode = "n",
-  prefix = "<leader>",
-  buffer = nil,
-  silent = true,
-  noremap = true,
-  nowait = false,
+	mode = "n",
+	prefix = "<leader>",
+	buffer = nil,
+	silent = true,
+	noremap = true,
+	nowait = false,
 }
 
 local mappings = {
+	g = {
+		name = "Git",
+		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+		u = {
+			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+			"Undo Stage Hunk",
+		},
+		o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+		c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+		d = {
+			"<cmd>Gitsigns diffthis HEAD<cr>",
+			"Diff",
+		},
+	},
 }
 
 whichkey.setup(conf)
-whichkey.register(mappings,opts)
+whichkey.register(mappings, opts)
